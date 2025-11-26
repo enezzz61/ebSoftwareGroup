45763491f0c7961101ff9a2654280cecdb0a6602
Mustafa Enes BAYRAM <153430719+enezzz61@users.noreply.github.com>
2025-11-26 16:50:55 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,19 @@
 export default function sitemap() {
   return [
     {
-      url: "https://seninsiteadresi.netlify.app",
+      url: "https://ebsoftwaregroup.netlify.app",
       lastModified: new Date(),
     },
     {
-      url: "https://seninsiteadresi.netlify.app/#hizmetler",
+      url: "https://ebsoftwaregroup.netlify.app/#hizmetler",
       lastModified: new Date(),
     },
     {
-      url: "https://seninsiteadresi.netlify.app/#projeler",
+      url: "https://ebsoftwaregroup.netlify.app/#projeler",
       lastModified: new Date(),
     },
     {
-      url: "https://seninsiteadresi.netlify.app/#iletisim",
+      url: "https://ebsoftwaregroup.netlify.app/#iletisim",
       lastModified: new Date(),
     }
   ];
