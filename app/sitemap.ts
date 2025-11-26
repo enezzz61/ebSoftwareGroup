74074472da82447d85099d15c120cd53217019ff
Mustafa Enes BAYRAM <153430719+enezzz61@users.noreply.github.com>
2025-11-26 16:50:02 +0300

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://seninsiteadresi.netlify.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://seninsiteadresi.netlify.app/#hizmetler",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://seninsiteadresi.netlify.app/#projeler",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://seninsiteadresi.netlify.app/#iletisim",
+      lastModified: new Date(),
+    }
+  ];
+}
